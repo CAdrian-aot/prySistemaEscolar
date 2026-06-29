b16638ea70eb9a9e0081da06deb7793276e3ceb2
Eduhdzxx-afk <20251063@uthh.edu.mx>
2026-06-29 09:56:39 -0600

    ya quedo mi parte

---
+++ prySistemaEscolar/clsAlumnos.cs
@@ -0,0 +1,70 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySistemaEscolar
+{
+    internal class clsAlumnos
+    {
+        private int matricula;
+        private string nombreAlumno;
+        private string apellidoP;
+        private string apellidoM;
+        private string direccion;
+        private string telefono;
+        private string correo;
+        private decimal promedioBachillerato;
+        private int idTutor; // Foraneo
+        private int idCarrera; // Foraneo
+        private int idusuario;
+
+        // Adaptador y tabla virtuales de la clase 
+        private MySqlDataAdapter consulta;
+        private DataTable tabla;
+
+        // Propiedades
+        public int Matricula { get => matricula; set => matricula = value; }
+        public string NombreAlumno { get => nombreAlumno; set => nombreAlumno = value; }
+        public string ApellidoP { get => apellidoP; set => apellidoP = value; }
+        public string ApellidoM { get => apellidoM; set => apellidoM = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public decimal PromedioBachillerato { get => promedioBachillerato; set => promedioBachillerato = value; }
+        public int IdTutor { get => idTutor; set => idTutor = value; }
+        public int IdCarrera { get => idCarrera; set => idCarrera = value; }
+        public int Idusuario { get => idusuario; set => idusuario = value; }
+
+        //Metodo para cargar datos en el DataGrid//
+        public DataTable CargarDataGrid()
+        {
+            tabla = new DataTable();
+
+            try
+            {
+                ClsConexion conexionBD = new ClsConexion();
+
+                using (var conexion = conexionBD.AbrirConexion())
+                {
+                    string sql = "SELECT A.matricula AS Matricula, A.nombreAlumno AS Nombre,\r\nA.apellidoP AS 'A. Paterno', A.apellidoM AS 'A. Materno',\r\nC.nombreCarrera AS Carrera,\r\nT.nombreTutor AS Tutor,\r\nU.vchnombreUsuario AS Usuario, \r\nA.direccion, A.telefono, A.correo, A.promedioBachillerato, A.idTutor, A.idCarrera, A.idUsuario\r\nFROM tblalumnos A\r\nINNER JOIN tblcarreras C ON A.idCarrera = C.idCarrera\r\nINNER JOIN tbltutores T ON A.idTutor = T.idTutor\r\nINNER JOIN tblusuarios U ON A.idUsuario = U.intidUsuario;";
+                    using (consulta = new MySqlDataAdapter(sql, conexion))
+                    {
+                        consulta.Fill(tabla);
+                    }//Liberar la consulta
+                }//Liberar conexión
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error en la conexión" + ex.Message);
+            }
+            return tabla;
+
+
+        }
+
+    }
+}
